f9cdc50f51a995ae7658222502669801bd018188
Martin Sotir <martinsotir@gmail.com>
2014-06-09 01:49:16 +0200

    InitGame State (give a tokenSet to every players).

--- ia04_project/poker/src/sma/agent/simulationAgent/InitGameBhv.java
+++ ia04_project/poker/src/sma/agent/simulationAgent/InitGameBhv.java
@@ -1,26 +1,27 @@
 package sma.agent.simulationAgent;
 
+import jade.core.AID;
+import jade.core.behaviours.ParallelBehaviour;
+import poker.game.player.model.Player;
 import sma.agent.SimulationAgent;
-import jade.core.behaviours.Behaviour;
+import sma.agent.helper.DFServiceHelper;
+import sma.agent.helper.TransactionBhv;
+import sma.message.Message;
+import sma.message.environment.request.GiveTokenSetToPlayerRequest;
 
-public class InitGameBhv extends Behaviour {
-	
-	private SimulationAgent simAgent;
+/**
+ * Start a new game, give every players a token set.
+ */
+public class InitGameBhv extends ParallelBehaviour {
 
 	public InitGameBhv(SimulationAgent simAgent) {
-		// TODO Auto-generated constructor stub
+		// The parallel behavior give every players a tokenSet (based
+		// the default distribution in simAgent).
+		super(simAgent, ParallelBehaviour.WHEN_ALL);
+		AID environment = DFServiceHelper.searchService(simAgent, "PokerEnvironment", "Environment");
+		for (Player p : simAgent.getGame().getGamePlayers()){
+			Message msg = new GiveTokenSetToPlayerRequest(simAgent.getDefaultTokenSet(), p.getAID());
+			this.addSubBehaviour(new TransactionBhv(simAgent, msg, environment));
+		}
 	}
-
-	@Override
-	public void action() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean done() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
