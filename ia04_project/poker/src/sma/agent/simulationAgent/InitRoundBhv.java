cfe5e78cc8a28758df8029fd4ce83d9989700d38
Martin Sotir <martinsotir@gmail.com>
2014-06-15 01:50:42 +0200

    fixed simulation env. watcher

--- ia04_project/poker/src/sma/agent/simulationAgent/InitRoundBhv.java
+++ ia04_project/poker/src/sma/agent/simulationAgent/InitRoundBhv.java
@@ -4,36 +4,33 @@ import sma.agent.SimulationAgent;
 import jade.core.behaviours.Behaviour;
 
 public class InitRoundBhv extends Behaviour {
-	
-	private SimulationAgent simAgent;
 
-	public InitRoundBhv(SimulationAgent simAgent) {
-		super(simAgent);
-		this.simAgent = simAgent;
-		
-		
-	}
+    private SimulationAgent simAgent;
 
-	@Override
-	public void action() {
-		// TODO Auto-generated method stub
+    public InitRoundBhv(SimulationAgent simAgent) {
+	super(simAgent);
+	this.simAgent = simAgent;
+    }
 
-	}
+    @Override
+    public void action() {
+	// TODO Auto-generated method stub
+    }
 
-	@Override
-	public boolean done() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
-	/**
-	 * Transition: 
-	 * Return the PLAY transition code.
-	 */
-	@Override
-	public int onEnd(){
-		return SimulationAgent.GameEvent.PLAY.ordinal();
-		
-	}
+    @Override
+    public boolean done() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    /**
+     * Transition: 
+     * Return the PLAY transition code.
+     */
+    @Override
+    public int onEnd(){
+	return SimulationAgent.GameEvent.PLAY.ordinal();
+
+    }
 
 }
