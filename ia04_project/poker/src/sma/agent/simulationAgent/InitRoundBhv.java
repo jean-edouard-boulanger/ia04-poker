2e3c9c30ab1e3bdbb4658662f27c257971c7cd6c
Martin Sotir <martinsotir@gmail.com>
2014-06-15 19:14:06 +0200

    fixed simuAgent behaviours

--- ia04_project/poker/src/sma/agent/simulationAgent/InitRoundBhv.java
+++ ia04_project/poker/src/sma/agent/simulationAgent/InitRoundBhv.java
@@ -7,43 +7,50 @@ import sma.agent.SimulationAgent;
 import sma.agent.helper.DFServiceHelper;
 import sma.agent.helper.SimpleVisitor;
 import sma.agent.helper.TransactionBhv;
-import sma.agent.helper.experimental.Task;
 import sma.agent.helper.experimental.TaskRunnerBhv;
 import sma.message.Message;
 import sma.message.dealer.request.DealRequest;
 
 public class InitRoundBhv extends TaskRunnerBhv {
 
-    private AID dealerAgent;
+	private AID dealerAgent;
+	private SimulationAgent simAgent;
 
-    public InitRoundBhv(SimulationAgent simAgent) {
-	super(simAgent);
-	this.dealerAgent = DFServiceHelper.searchService(simAgent, "DealerAgent","Dealer");
-	
-	// we update the current round:
-	simAgent.setCurrentRound(simAgent.getCurrentRound().getNext()); //TODO: maybe we need to notify the environment about that
-	
-	// we deal community cards
-	setBehaviour(dealCommunityCardsBhv(simAgent.getCurrentRound())); 
-    }
-    
-    private Behaviour dealCommunityCardsBhv(Round round) {
-	Message msg = new DealRequest(round);
-	TransactionBhv transaction = new TransactionBhv(myAgent, msg, dealerAgent);
-	transaction.setResponseVisitor(new SimpleVisitor(myAgent,
-		"community cards dealt successfully.",
-		"error while dealing community cards."));		
-	return transaction;
-    }
-        
-    /**
-     * Transition: 
-     * Return the PLAY transition code.
-     */
-    @Override
-    public int onEnd(){
-	return SimulationAgent.GameEvent.PLAY.ordinal();
+	public InitRoundBhv(SimulationAgent simAgent) {
+		super(simAgent);
+		this.dealerAgent = DFServiceHelper.searchService(simAgent, "DealerAgent","Dealer");
+		this.simAgent = simAgent;
+	}
 
-    }
+	@Override
+	public void onStart() {
+
+		// we update the current round:
+		simAgent.setCurrentRound(simAgent.getCurrentRound().getNext()); //TODO: maybe we need to notify the environment about that
+
+		// we deal community cards
+		setBehaviour(dealCommunityCardsBhv(simAgent.getCurrentRound())); 
+
+		super.onStart();
+	}
+
+	private Behaviour dealCommunityCardsBhv(Round round) {
+		Message msg = new DealRequest(round);
+		TransactionBhv transaction = new TransactionBhv(myAgent, msg, dealerAgent);
+		transaction.setResponseVisitor(new SimpleVisitor(myAgent,
+				"community cards dealt successfully.",
+				"error while dealing community cards."));		
+		return transaction;
+	}
+
+	/**
+	 * Transition: 
+	 * Return the PLAY transition code.
+	 */
+	@Override
+	public int onEnd(){
+		return SimulationAgent.GameEvent.PLAY.ordinal();
+
+	}
 
 }
