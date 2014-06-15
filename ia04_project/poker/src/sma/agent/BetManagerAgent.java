05f1dd132f178d61a0c1e7d68a675c0445780485
Maxime <final7765@gmail.com>
2014-06-15 14:23:25 +0200

    Bet Request.

--- ia04_project/poker/src/sma/agent/BetManagerAgent.java
+++ ia04_project/poker/src/sma/agent/BetManagerAgent.java
@@ -1,26 +1,33 @@
 package sma.agent;
 
+import gui.player.PlayerWindow.PlayerGuiEvent;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import poker.game.model.BetContainer;
+import poker.game.model.Game;
 import poker.game.model.PlayersContainer;
+import poker.game.player.model.Player;
 import sma.agent.helper.AgentHelper;
 import sma.agent.helper.DFServiceHelper;
 import sma.agent.helper.TransactionBhv;
 import sma.message.FailureMessage;
 import sma.message.MessageVisitor;
 import sma.message.SubscriptionOKMessage;
+import sma.message.bet.request.BetRequest;
+import sma.message.environment.notification.PlayerReceivedTokenSetNotification;
 
 public class BetManagerAgent extends Agent {
 
 	BetContainer betContainer;
-	PlayersContainer playersContainer;
 	BetManagerMessageVisitor msgVisitor;
+	Game game;
 	
 	public BetManagerAgent(){
 		super();
+		betContainer = new BetContainer();
+		game = new Game();
 	}
 	
 	public void setup(){
@@ -73,6 +80,22 @@ public class BetManagerAgent extends Agent {
 	
 	private class BetManagerMessageVisitor extends MessageVisitor {	
 		
+		@Override
+		public boolean onPlayerReceivedTokenSetNotification(PlayerReceivedTokenSetNotification notification, ACLMessage aclMsg){
+			
+			Player player = game.getPlayersContainer().getPlayerByAID(notification.getPlayerAID());
+			player.setTokens(notification.getReceivedTokenSet());
+			
+			return true;
+		}
+		
+		@Override
+		public boolean onBetRequest(BetRequest request, ACLMessage aclMsg) {
+			
+			
+			
+			return true;
+		}
 	}
 
 	public BetManagerMessageVisitor getMsgVisitor() {
--- ia04_project/poker/src/sma/message/bet/request/BetRequest.java
+++ ia04_project/poker/src/sma/message/bet/request/BetRequest.java
@@ -1,16 +1,52 @@
 package sma.message.bet.request;
 
+import jade.core.AID;
 import jade.lang.acl.ACLMessage;
+import poker.token.model.TokenSet;
 import sma.message.Message;
 import sma.message.MessageVisitor;
 
 public class BetRequest extends Message {
 
+	private int bet;
+	private TokenSet tokenSet;
+	private AID player;
+	
 	public BetRequest(){}
+	
+	public BetRequest(int bet, TokenSet tokenSet, AID player) {
+		this.bet = bet;
+		this.tokenSet = tokenSet;
+		this.player = player;
+	}
 	
 	@Override
 	public boolean accept(MessageVisitor visitor, ACLMessage aclMsg) {
 		return visitor.onBetRequest(this, aclMsg);
+	}
+
+	public int getBet() {
+		return bet;
+	}
+
+	public void setBet(int bet) {
+		this.bet = bet;
+	}
+
+	public TokenSet getTokenSet() {
+		return tokenSet;
+	}
+
+	public void setTokenSet(TokenSet tokenSet) {
+		this.tokenSet = tokenSet;
+	}
+
+	public AID getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(AID player) {
+		this.player = player;
 	}
 
 }
