b2405d76b63081b5abeff75d88d3c03b0aabc72d
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2014-06-15 11:09:27 +0200

    Changed player icon in IHM

--- ia04_project/poker/src/gui/player/PersoIHM.java
+++ ia04_project/poker/src/gui/player/PersoIHM.java
@@ -21,22 +21,29 @@ import javafx.util.Duration;
 public class PersoIHM extends Group {
 	
 	private Label name;
-	private Circle circle;
 	
 	private String pseudo;
 	
 	private ImageView image_current_player;
+	private ImageView image_player;
 	
 	Timeline t_current_player;
 	
 	public enum Sens { GAUCHE, DROITE, HAUT, BAS }
 	public PersoIHM(int x, int y, String pseudo, Sens position)
 	{
-		circle = new Circle();
-		circle.setRadius(15);
-		circle.setFill(Color.AQUA);
-		circle.setCenterX(x);
-		circle.setCenterY(y);
+
+		
+		image_player = new ImageView(new Image("images/player_other.png"));
+		image_player.setLayoutX(x - image_player.getBoundsInLocal().getWidth() / 2);
+		image_player.setLayoutY(y - image_player.getBoundsInLocal().getWidth() / 2);
+		image_player.setVisible(true);
+		
+		image_current_player = new ImageView(new Image("images/player_active_background.png"));
+		image_current_player.setLayoutX(x - image_current_player.getBoundsInLocal().getWidth() / 2);
+		image_current_player.setLayoutY(y - image_current_player.getBoundsInLocal().getWidth() / 2);
+		image_current_player.setVisible(false);
+		
 		
 		name = new Label(pseudo + " (0)");
 
@@ -44,46 +51,35 @@ public class PersoIHM extends Group {
 		
 		this.pseudo = pseudo;
 		
-		image_current_player = new ImageView(new Image("images/current_player.png"));
-		image_current_player.setVisible(false);
-		
 		t_current_player = new Timeline();
 		
 		if(position.equals(Sens.HAUT))
 		{
 			name.setLayoutX(x-20);
 			name.setLayoutY(y-40);
-			image_current_player.setLayoutX(x-40);
-			image_current_player.setLayoutY(y-40);
 		}
 		
 		if(position.equals(Sens.GAUCHE))
 		{
 			name.setLayoutX(x-20);
 			name.setLayoutY(y+20);
-			image_current_player.setLayoutX(x-40);
-			image_current_player.setLayoutY(y+20);
 		}
 		
 		if(position.equals(Sens.DROITE))
 		{
 			name.setLayoutX(x-20);
 			name.setLayoutY(y+20);
-			image_current_player.setLayoutX(x-40);
-			image_current_player.setLayoutY(y+20);
 		}
 		
 		if(position.equals(Sens.BAS))
 		{
 			name.setLayoutX(x-20);
 			name.setLayoutY(y+25);
-			image_current_player.setLayoutX(x-40);
-			image_current_player.setLayoutY(y+25);
 		}
 		
-		this.getChildren().add(circle);
 		this.getChildren().add(name);
 		this.getChildren().add(image_current_player);
+		this.getChildren().add(image_player);
 		
 		setAnimationCurrentPlayer();
 	}
@@ -124,7 +120,7 @@ public class PersoIHM extends Group {
 	
 	public void setMe()
 	{
-		circle.setFill(Color.GREEN);
+		image_player.setImage(new Image("images/player_me.png"));
 	}
 	
 	public void setCurrentPlayer()
@@ -144,6 +140,10 @@ public class PersoIHM extends Group {
 		name.setText(pseudo + " (" + score + ")");
 	}
 	
+	/*
+	 * Regarde poker.token.helpers.TokenSetValueEvaluator
+	 * Il calcule a valeur d'une main à partir d'un TokenSet, et de la definition de la valeur des jetons
+	 */
 	public static int calculateScore(TokenSet token_set)
 	{
 		int score = 0;
