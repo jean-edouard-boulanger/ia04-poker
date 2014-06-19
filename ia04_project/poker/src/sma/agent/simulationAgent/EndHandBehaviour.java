9c707fb23bf4291316af4ac5ff97a00d37447c32
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2014-06-19 02:06:35 +0200

    Started to implement the EndHandBehaviour

--- ia04_project/poker/src/sma/agent/simulationAgent/EndHandBehaviour.java
+++ ia04_project/poker/src/sma/agent/simulationAgent/EndHandBehaviour.java
@@ -1,15 +1,50 @@
 package sma.agent.simulationAgent;
 
+import jade.core.AID;
 import jade.core.Agent;
 import sma.agent.SimulationAgent;
+import sma.agent.SimulationAgent.GameEvent;
+import sma.agent.helper.DFServiceHelper;
+import sma.agent.helper.TransactionBehaviour;
+import sma.agent.helper.experimental.Task;
 import sma.agent.helper.experimental.TaskRunnerBehaviour;
+import sma.message.environment.request.EmptyCommunityCardsRequest;
 
 public class EndHandBehaviour extends TaskRunnerBehaviour {
 
 	private SimulationAgent simulationAgent;
+	private AID environment;
 	
-	public EndHandBehaviour(SimulationAgent agent) {
-		super(agent);
+	public EndHandBehaviour(SimulationAgent simulationAgent) {
+		super(simulationAgent);
 		this.simulationAgent = simulationAgent;
+		this.environment = DFServiceHelper.searchService(simulationAgent, "PokerEnvironment", "Environment");
 	}
+	
+	public void onStart(){
+		
+		Task mainTask = Task.New(this.getEmptyCommunityCardsBehaviour())
+				.then(this.getEmptyPotBehaviour());
+		
+		this.setBehaviour(mainTask);
+		super.onStart();
+	}
+	
+	public int onEnd(){
+		return GameEvent.NEW_HAND.ordinal();
+	}
+	
+	public TransactionBehaviour getEmptyCommunityCardsBehaviour(){
+		EmptyCommunityCardsRequest emptyCommunityCardsRequest = new EmptyCommunityCardsRequest();
+		TransactionBehaviour transactionBehaviour = new TransactionBehaviour(simulationAgent, 
+				emptyCommunityCardsRequest, environment);
+		return transactionBehaviour;
+	}
+	
+	public TransactionBehaviour getEmptyPotBehaviour(){
+		TransactionBehaviour transactionBehaviour = new TransactionBehaviour(simulationAgent, 
+				emptyCommunityCardsRequest, environment);
+		return transactionBehaviour;
+	}
+	
 }
